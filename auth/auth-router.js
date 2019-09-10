060508f998a12fe212d8e7b66dc21e7a0dcfaefe
cwern40 <chris.wernli@hotmail.com>
2019-09-09 19:20:37 -0600

    adding guided project code

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -11,9 +12,11 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      const token = generateToken(saved);
+      res.status(201).json({saved, token});
     })
     .catch(error => {
+      console.log(error);
       res.status(500).json(error);
     });
 });
@@ -25,8 +28,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +42,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    role: user.role
+  };
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options)
+}
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -2,13 +2,23 @@ const router = require('express').Router();
 
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
+const { sub, role } = req.decodedToken;
 
 router.get('/', restricted, (req, res) => {
+
+  if (role === 'admin') {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+  } else {
+    Users.findById(sub)
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => )
+  }
 });
 
 module.exports = router;
